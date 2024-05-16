7eeeba49e970bfa71ca05c0aa38d4084b6e72937
Nikoo Ghafouri <nikooghafouri@Nikoos-MacBook-Air.local>
2024-05-16 19:38:48 -0400

    doFizzBuzz method slightly tweaked

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,11 +8,11 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
             i = doFizzBuzz(i);
+            i++;
         }
     }
 
     private static int doFizzBuzz(int i) {
-        i++;
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
